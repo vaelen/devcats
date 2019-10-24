6690b7586588ab8ee9570fb9498e4429a8b7cda6
Andrew Young <andrew@vaelen.org>
2019-10-25 08:07:04 +0900

    Fix license

--- devcats.c
+++ devcats.c
@@ -3,12 +3,12 @@ This file is part of devcats.
 
 Copyright 2017, Andrew Young <andrew@vaelen.org>
 
-    Vaelen/DB is free software: you can redistribute it and/or modify
+    Devcats is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-    Vaelen/DB is distributed in the hope that it will be useful,
+    Devcats is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
